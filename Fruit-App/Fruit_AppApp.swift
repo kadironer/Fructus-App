9dadf1b34a970981bcdf8bc09fb8358409dd1a7b
kadironer <kadironer46500@gmail.com>
2023-04-02 22:56:59 +0300

    Initial Commit

---
+++ Fruit-App/Fruit_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruit_AppApp.swift
+//  Fruit-App
+//
+//  Created by Kadir Ö. Macbook Air UX-UI Designer on 2.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Fruit_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

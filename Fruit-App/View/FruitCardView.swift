e2eff62e16b710558fa8d4cbc2a3e3b39a3f9777
kadironer <kadironer46500@gmail.com>
2023-04-03 21:36:37 +0300

    Ready project folder

---
+++ Fruit-App/View/FruitCardView.swift
@@ -0,0 +1,21 @@
+//
+//  FruitCardView.swift
+//  Fruit-App
+//
+//  Created by Kadir Ö. Macbook Air UX-UI Designer on 3.04.2023.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text("Blueberry")
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
